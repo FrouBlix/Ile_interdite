0f203428756365c9687ce652ddcf462b08e427da
olivier <oliviersenn6@gmail.com>
2018-05-31 09:32:43 +0200

    finished Tuile

--- src/ile_interdite/Tuile.java
+++ src/ile_interdite/Tuile.java
@@ -10,14 +10,45 @@ package ile_interdite;
  * @author reboulef
  */
 public class Tuile {
-    private String nom;
+
+ 
+    private EtatsTuiles etat;
+    private final Special special;
+    private final String nom;
     private Coordonnees coordonnees;
     
+    public Special getSpecial() {
+        return special;
+    }
+
+    public EtatsTuiles getEtat() {
+        return etat;
+    }
+
+    public void setEtat(EtatsTuiles etat) {
+        this.etat = etat;
+    }
+
+    public Tuile(Special special, String nom, Coordonnees coordonnees) {
+        this.special = special;
+        this.nom = nom;
+        this.coordonnees = coordonnees;
+        this.setEtat(EtatsTuiles.seche); // une tuile nouvellement cree est forcement seche
+    }
+
     public Coordonnees getCoordonnees(){
         return this.coordonnees;
     }
     
     public String getNom(){
         return this.nom;
+    }
+    
+       @Override
+    public String toString() {
+        return "Tuile :" + this.getCoordonnees() + "\n"
+                + "\tNom:" + this.getNom()+"\n"
+                + "\tEtat:" + this.getEtat()+"\n"
+                + "\tSpecial:" + this.getSpecial();
     }
 }
