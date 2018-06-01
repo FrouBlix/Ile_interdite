5d32c38481e3216b69d5ba9804cdb5dade00eee2
olivier <oliviersenn6@gmail.com>
2018-06-01 13:36:41 +0200

    added autoadd and regular add

--- src/ile_interdite/Grille.java
+++ src/ile_interdite/Grille.java
@@ -5,6 +5,7 @@
  */
 package ile_interdite;
 
+import java.net.CookieHandler;
 import java.util.HashMap;
 
 /**
@@ -15,11 +16,39 @@ public class Grille {
 
     public Grille() {
         this.tuiles = new HashMap<>();
+        this.tuiles.put(new Coordonnees(0, 0), null); //remplir les coins avec des null
+        this.tuiles.put(new Coordonnees(0, 1), null);
+        this.tuiles.put(new Coordonnees(1, 0), null);
+        this.tuiles.put(new Coordonnees(0, 4), null);
+        this.tuiles.put(new Coordonnees(0, 5), null);
+        this.tuiles.put(new Coordonnees(1, 5), null);
+        this.tuiles.put(new Coordonnees(4, 0), null);
+        this.tuiles.put(new Coordonnees(5, 0), null);
+        this.tuiles.put(new Coordonnees(5, 1), null);
+        this.tuiles.put(new Coordonnees(4, 5), null);
+        this.tuiles.put(new Coordonnees(5, 4), null);
+        this.tuiles.put(new Coordonnees(5, 5), null);
     }
     private HashMap<Coordonnees,Tuile> tuiles;
     
     public void addTuile(Tuile tuile){
         this.tuiles.put(tuile.getCoordonnees(), tuile);
+    }
+    
+    public void autoAddTuile(Tuile tuile) throws Exception{
+        Coordonnees coords = new Coordonnees(0, 0);
+        for (int i = 0; i < 5; i++) {
+            coords.setX(i);
+            for (int j = 0; j < 5; j++) {
+                coords.setY(j);
+                if (!this.tuiles.containsKey(coords)) {
+                    tuile.setCoordonnees(coords);
+                    this.addTuile(tuile);
+                    return;
+                }
+            }
+        }
+        throw new Exception("Grille: sorti de la boucle en ajoutant " + tuile);
     }
     
 }
