dd1e4edcd662ab52784a3722cca3d5b57eb454f7
FrouBlix <froublix@gmail.com>
2018-06-22 21:52:43 +0200

    j'arrive pas à faire apparaitres les boutons et a changer les couleurs

--- src/ile_interdite/IHMFin.java
+++ src/ile_interdite/IHMFin.java
@@ -6,54 +6,86 @@
 package ile_interdite;
 
 import GraphicsUtil.ImagePanel;
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextArea;
 
 /**
  *
  * @author Félix Reboulet
  */
 public class IHMFin extends JFrame{
-    private JLabel fetreFin;
-    private JLabel messageFin;
+    private JTextArea messageFin;
     private JLabel titreMessage;
     private ImagePanel image;
     private JButton quitter;
     private JButton menu;
     
 
-    public IHMFin(){
+    public IHMFin(int cas){
         super("Fin de partie");
+        this.setSize(1300, cas < 5 ? 390 : 460);
         
+        switch (cas){
+            case 5:
+                messageFin = new JTextArea("Félicitatons vous avez récuperé tous les trésors\net vous avez réussi à quitter l'île !!\nVotre mission est un succès.");
+            break;
+            
+            case 4:
+                messageFin = new JTextArea("Le compteur de la montée des eaux a atteint son paroxysme,\nvous avez échoué.");
+                break;
+                
+            case 3:
+                messageFin = new JTextArea("L'un des Trésor a sombré avec l'île,\nvous ne pouvez donc plus le récupérer.\nVotre mission est un échec.");
+                break;
+                
+            case 2:
+                messageFin = new JTextArea("L'Héliport a sombré avec l'île ,\nvous ne pouvez plus vous échapper.\nVous êtes comdamner.");
+                break;
+                
+            case 1:
+                messageFin = new JTextArea("L'un des aventurier s'est noyer, vous avez échoué.\nVotre mission est un échec.");
+                break;
+                
+            default:
+                break;
+            
+        }
+        
+        messageFin.setLineWrap(true);
+        messageFin.setBackground(Color.LIGHT_GRAY);
         this.setLayout(new GridLayout(1,2));
-        
+
         JLabel information = new JLabel();
-        information.setLayout(new GridLayout(3,1));
-        
+        information.setLayout(new BorderLayout());
+
         JLabel boutons = new JLabel();
-        boutons.setLayout(new GridLayout(1,2));
+        boutons.setLayout(new BorderLayout());
         
-        image = new ImagePanel("image-de-Défaite.jpg",1,false);
-        titreMessage = new JLabel("VICOTORY");
-        messageFin = new JLabel("ouais ouais ouais c'est gagné");
+        image = new ImagePanel(cas < 5 ? "image-de-Défaite.jpg" : "image-de-Victoire.jpg", cas < 5 ? 1.25 : 1.50);
+        titreMessage = new JLabel(cas < 5 ? "DEFAITE" : "VICTOIRE");
+        information.setBackground(Color.LIGHT_GRAY);
+        this.setBackground(Color.PINK);
         
         quitter = new JButton("QUITTER");
         menu = new JButton("MENU");
-        
-        boutons.add(quitter);
-        boutons.add(menu);
-        
-        information.add(titreMessage);
-        information.add(messageFin);
-        information.add(boutons);
+
+
+        information.add(titreMessage, BorderLayout.NORTH);
+        information.add(messageFin, BorderLayout.CENTER);
+        information.add(boutons, BorderLayout.SOUTH);
+
+        boutons.add(menu, BorderLayout.WEST);
+        boutons.add(quitter, BorderLayout.EAST);
         
         this.add(image);
         this.add(information);
-        
-        this.setVisible(false);
-        
+
+
     }
 }
